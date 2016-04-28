af65cf7264b284b94957af590c98ccccdf1205e3
burt <skyfe79@gmail.com>
2016-04-28 15:58:21 +0900

    add animation callbacks

--- app/src/main/java/kr/pe/burt/android/lib/faimageview/app/SplashActivity.java
+++ app/src/main/java/kr/pe/burt/android/lib/faimageview/app/SplashActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,12 +23,36 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         faImageView = (FAImageView)findViewById(R.id.faimageview);
-        faImageView.setInterval(1000);
+        faImageView.setInterval(5000);
         faImageView.setLoop(true);
         faImageView.addImageFrame(R.drawable.number01);
         faImageView.addImageFrame(R.drawable.number02);
         faImageView.addImageFrame(R.drawable.number03);
 
+        faImageView.setOnStartAnimationListener(new FAImageView.OnStartAnimationListener() {
+            @Override
+            public void onStartAnimation() {
+                Log.v("FAImageView", "Animation started");
+            }
+        });
+
+        faImageView.setOnFinishAnimationListener(new FAImageView.OnFinishAnimationListener() {
+            @Override
+            public void onFinishAnimation(boolean isLoopAnimation) {
+                if(isLoopAnimation) {
+                    Log.v("FAImageView", "Finish an animation cycle");
+                } else {
+                    Log.v("FAImageView", "Animation Finished");
+                }
+            }
+        });
+
+        faImageView.setOnFrameChangedListener(new FAImageView.OnFrameChangedListener() {
+            @Override
+            public void onFrameChanged(int index) {
+                Log.v("FAImageView", String.format("frame has changed %d", index));
+            }
+        });
     }
 
     @Override
@@ -43,7 +68,8 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     void onNextButtonClicked(View sender) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+        faImageView.startAnimation();
     }
 }
